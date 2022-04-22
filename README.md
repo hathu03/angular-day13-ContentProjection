f004e121c79d396437b133067dd5c582ec8798c9
hathu03 <nguyenthuha2411vmh@gmail.com>
2022-04-22 18:18:54 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-pqgtjq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-pqgtjq)
---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular';
+  questions = {
+    question1: true,
+    question2: false
+  }
+}
